426759bbfed570fa3feb1d211d7d2f1e56af47ff
Stepan_nout <Stepanmaslov@gmail.com>
2023-09-14 15:12:00 +0300

    почти готова форма регистрации

--- TestLogin/Registration.xaml.cs
+++ TestLogin/Registration.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 
 namespace TestLogin
 {
@@ -31,22 +34,82 @@ namespace TestLogin
             string pass = PasswordTextBox.Password;
             string passRep = PasswordRepeatTextBox.Password;
 
-            if (login.Length < 5)
+            bool chekLogin = ChekLogin(login);
+            bool chekPass = ChekPass(pass, passRep);
+
+            if (chekLogin && chekPass)
             {
-                ToolTip toolTip = new ToolTip();
-                toolTip.Content = "Недопустимая длинна";
-                toolTip.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
-                LoginTextBox.ToolTip = toolTip;
-                LoginTextBox.Foreground = Brushes.Red;
-                LoginTextBox.BorderBrush = Brushes.DarkRed;
+                textForRootPage.Text = "Успех";
+            }
+        }
+
+        private bool ChekLogin(string username)
+        {
+            if (username.Length < 5)
+            {
+                NoCorectInp("Логин слишком короткий", ref LoginTextBox);
+                return false;
             }
             else
             {
-                LoginTextBox.Foreground = Brushes.Green;
-                LoginTextBox.BorderBrush = Brushes.DarkGreen;
+                CorectInp(ref LoginTextBox);
+                return true;
+            }
+        }
+
+        private bool ChekPass(string pass, string repPass)
+        {
+            if (pass.Length < 5)
+            {
+                NoCorectInp("Пароль слишком короткий", ref PasswordTextBox);
+                return false;
+            }
+            else if (!Regex.IsMatch(pass, @"[a-zA-Z]+"))
+            {
+                NoCorectInp("Пароль должен содержать латинские символы", ref PasswordTextBox);
+                return false;
+            }
+            else if (!Regex.IsMatch(pass, @"\d+"))
+            {
+                NoCorectInp("Пароль должен содержать не менее 1ой цифры", ref PasswordTextBox);
+                return false;
+            }
+            else if (Regex.IsMatch(pass, @"\s+"))
+            {
+                NoCorectInp("Пароль не должен содержать пробелы", ref PasswordTextBox);
+                return false;
+            }
+            else
+            {
+                CorectInp(ref PasswordTextBox);
             }
 
+             if (pass != repPass)
+            {
+                NoCorectInp("Пароль не совпадает", ref PasswordRepeatTextBox);
+                return false;
+            } else CorectInp(ref PasswordRepeatTextBox);
+           
+            return true;
+        }
+        private void NoCorectInp<T>(string mess, ref T box) where T : Control
+        {
 
+            ToolTip toolTip = new ToolTip();
+            toolTip.Content = mess;
+            toolTip.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
+
+            box.ToolTip = toolTip;
+            box.Foreground = Brushes.Red;
+            box.BorderBrush = Brushes.DarkRed;
+        }
+
+        private void CorectInp<T>(ref T box)  where T : Control
+        {
+
+            box.ToolTip = null;
+            box.Foreground = Brushes.Green;
+            box.BorderBrush = Brushes.DarkGreen;
         }
     }
 }
