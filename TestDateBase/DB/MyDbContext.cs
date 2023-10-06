4d880836d7a3ee12340023637c4607904a20dca6
Степан Маслов <Stepanmaslov@gmail.com>
2023-10-06 15:45:06 +0300

    не работает создание бд

---
+++ TestDateBase/DB/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using TestDateBase.DB.Tabels;
+
+namespace TestDateBase.DB
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DateBase") { }
+
+        public DbSet<People> Peoples { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ TestDateBase/Logics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDateBase
+{
+    public static class Logics
+    {
+
+        public static int InputInt()
+        {
+            bool flag = false;
+            int inputInt = -1;
+            while (!flag)
+            {
+                Console.WriteLine("введите число 1 - 4");
+                string inputStr = Console.ReadLine();
+
+                try
+                {
+                    inputInt = Int32.Parse(inputStr);
+                    if (1 <= inputInt && inputInt <= 4)
+                    {
+                        flag = true;
+                        return inputInt;
+                    }
+                }
+                catch
+                { }
+                Console.WriteLine("не подходит");
+
+
+            }
+
+            return inputInt;
+        }
+
+
+    }
+}
